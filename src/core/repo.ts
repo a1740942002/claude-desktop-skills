36f27294a22a40e25b11b686265729f53a8f3f16
a1740942002 <a1740942002@me.com>
2026-05-26 18:26:14 +0800

    fix: use named import for simple-git to fix TypeScript errors

--- src/core/repo.ts
+++ src/core/repo.ts
@@ -1,5 +1,5 @@
 import { join } from "path";
-import simpleGit from "simple-git";
+import { simpleGit } from "simple-git";
 import { existsSync } from "fs";
 import { REPOS_DIR } from "../constants.js";
 
